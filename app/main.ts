d20e4784631a93e435f0c8c104bedf0981858ff0
José Ricardo <josericardo.fleury05@gmail.com>
2022-12-27 15:42:41 -0300

    Added all weapons

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,7 @@ class Player {
   name: string;
 }
 class Weapon {
-  name: string;
+  id: string;
   price: number;
 }
 class Team {
@@ -32,12 +32,73 @@ const castro: Player = {
   name: "Gustavo",
 }
 
-// Weapons
+// ! Weapons
+// * Sidearms
+const classic: Weapon = {
+  id: "Classic",
+  price: 0,
+}
+const shorty: Weapon = {
+  id: "Shorty",
+  price: 150,
+}
+const frenzy: Weapon = {
+  id: "Frenzy",
+  price: 450,
+}
+const ghost: Weapon = {
+  id: "Ghost",
+  price: 500,
+}
+const sheriff: Weapon = {
+  id: "Sheriff",
+  price: 800,
+}
+
+// * SMGs
+const stinger: Weapon = {
+  id: "Stinger",
+  price: 950,
+}
+const spectre: Weapon = {
+  id: "Spectre",
+  price: 1600,
+}
+
+// * Shotguns
+const bucky: Weapon = {
+  id: "Bucky",
+  price: 850,
+}
+const judge: Weapon = {
+  id: "Judge",
+  price: 1850,
+}
+
+// * Rifles
+const bulldog: Weapon = {
+  id: "Bulldog",
+  price: 2050,
+}
+const guardian: Weapon = {
+  id: "Guardian",
+  price: 2250,
+}
 const phantom: Weapon = {
-  name: "Phantom",
+  id: "Phantom",
   price: 2900,
 }
 const vandal: Weapon = {
-  name: "Vandal",
+  id: "Vandal",
   price: 2900,
+}
+
+// * Sniper Rifles
+const marshall: Weapon = {
+  id: "Marshall",
+  price: 950,
+}
+const operator: Weapon = {
+  id: "Operator",
+  price: 4700,
 }
